5b3f93d6611fc7d7218b1d744ee4f4fa865cb6b8
Anatolii88 <aa.bezchastniy@gmail.com>
2023-03-24 15:30:44 +0200

    Create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li> */
+
+const galleryEl = document.querySelector('.gallery')
+
+
+function getGalleryItems(galleryItems) { 
+  
+  return galleryItems.map(element => {
+  return `<li class="gallery__item"><a class="gallery__link" href="${element.original}"><img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    /></a></li>`
+}).join('')
+}
+galleryEl.innerHTML= getGalleryItems(galleryItems)
