be65a76917b346c25fe64b40aa60e10afc9875a8
Anatolii Bezchastniy <aa.bezchastniy@gmail.com>
2023-03-25 21:01:45 +0200

    Finish task number one

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -30,3 +30,16 @@ function getGalleryItems(galleryItems) {
 }).join('')
 }
 galleryEl.innerHTML= getGalleryItems(galleryItems)
+
+galleryEl.addEventListener('click', onItemClick)
+function onItemClick(e) { 
+  e.preventDefault()
+  if (!e.target.classList.contains('gallery__image')) { 
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+
+`)
+  instance.show()
+}
